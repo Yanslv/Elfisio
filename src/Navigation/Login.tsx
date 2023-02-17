427da65797e72a2ac9498236966011ddb9299d35
Yan Carlos <yan.amorim@grupohpar.com.br>
2023-02-17 08:29:26 -0400

    Layout da tela de login

--- src/Navigation/Login.tsx
+++ src/Navigation/Login.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
-import {View,Text} from 'react-native'
+import {View, Text, TextInput, Button} from 'react-native';
 
 export const Login = () => {
-    return (
-        <View>
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#2b961f',
+        justifyContent: 'space-evenly',
+      }}>
+        <View style={{ justifyContent:"center", alignItems:"center" }}>
             <Text>
-                Login
+                LOGOTIPO
             </Text>
         </View>
-    )
-}
+      <View style={{justifyContent: 'space-around'}}>
+        <View style={{margin: 24, justifyContent: 'space-around'}}>
+          <TextInput
+            style={{backgroundColor: '#ffff', borderRadius: 4}}
+            placeholder="Digite seu nome"></TextInput>
+        </View>
+        <View style={{margin: 24, justifyContent: 'space-around'}}>
+          <TextInput
+            style={{backgroundColor: '#ffff', borderRadius: 4}}
+            placeholder="Digite sua senha"></TextInput>
+        </View>
+        <View style={{margin: 24}}>
+          <Button title="ENTRAR"></Button>
+        </View>
+      </View>
+    </View>
+  );
+};
